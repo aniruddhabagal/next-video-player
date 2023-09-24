7ed13c39616f48d55a395d300675f098f639b679
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-09-24 20:02:59 +0530

    Created Pages

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+import React from "react";
+import VideoThumbnail from "../components/VideoThumbnail";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <VideoThumbnail />
+    </div>
+  );
+};
+
+export default Home;
---
+++ pages/player.js
@@ -0,0 +1,13 @@
+import React from "react";
+import VideoPlayer from "../components/VideoPlayer";
+
+const Player = () => {
+  return (
+    <div>
+      <h1>Video Player</h1>
+      <VideoPlayer />
+    </div>
+  );
+};
+
+export default Player;
